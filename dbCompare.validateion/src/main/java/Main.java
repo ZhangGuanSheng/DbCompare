31d5708cfb14eb7577349d79b3fbb6cac0608a8b
zhanggs <502558513@qq.com>
2021-02-20 16:44:47 +0800

    1. read data from sql server database; 2. add dependence packages using maven plugin command line.

--- dbCompare.validateion/src/main/java/Main.java
+++ dbCompare.validateion/src/main/java/Main.java
@@ -1,8 +1,29 @@
+
+
+import java.util.List;
+import mybatis.entity.SysUserInfo;
+import mybatis.util.MyBatisutil;
+import org.apache.ibatis.session.SqlSession;
+
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello world.");
+        System.out.println("Test-Data:");
+
+        // 获取SqlSession对象
+        SqlSession session = MyBatisutil.getSqlSession();
+        // 查询全部对象
+        List<SysUserInfo> list = session.selectList("mybatis.entity.SysUserInfo.queryUsers");
+        System.out.println("总记录数：" + list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        //关闭SqlSession对象
+        MyBatisutil.closeSqlSession(session);
 
     }
 
